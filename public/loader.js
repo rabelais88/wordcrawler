54876c02332d4cb2fd9d6db53f966a7873360cfe
rabelais88 <sungryeolp@gmail.com>
2018-03-07 05:03:15 +0900

    further fix

--- public/loader.js
+++ public/loader.js
@@ -2,13 +2,21 @@ $(function(){
   $.getJSON('ajax.json',(res)=>{
     console.log(res);
     $('#time').html(res.time);
-    res.result.forEach((elRes)=>{
-      $('#wordrank').append(`<p><a href='https://search.naver.com/search.naver?query=${elRes[0]}' class='word' style='font-size:${15 + elRes[1] * 2}px;'>${elRes[0]} 빈도수 ${elRes[1]} 회</a></p>`);
-      $('#wordrank').append('<ul>');
-      elRes[2].forEach((elTitle)=>{
-        $('#wordrank').append(`<li>${elTitle}</li>`)
-      })
-      $('#wordrank').append('</ul>');
+    res.result.forEach((elRes,index)=>{
+      if(elRes[1] > 2){
+        $('#wordrank').append(`<p><a href='https://search.naver.com/search.naver?query=${elRes[0]}' class='word' style='font-size:${15 + elRes[1] * 2}px;'>${elRes[0]} - 빈도수 ${elRes[1]} 회</a></p>`);
+        $('#wordrank').append('<ul>');
+        elRes[2].forEach((elTitle)=>{
+          $('#wordrank').append(`<li>${elTitle}</li>`);
+        })
+        $('#wordrank').append('</ul>');
+      }else{
+        var etc = elRes[0];
+        if(index < res.result.length - 2){
+          etc += ' / '
+        }
+        $('#wordetc').append(`<span>${etc}</span>`)
+      }
     })
   })
 })
